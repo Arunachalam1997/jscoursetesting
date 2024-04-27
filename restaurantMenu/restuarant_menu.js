4bb355f5171f96d445253ae01579b18c8eb8e4cc
Arunachalam1997 <arunfriskio@gmail.com>
2024-04-27 01:08:26 -0400

    ResUpdate

---
+++ restaurantMenu/restuarant_menu.js
@@ -0,0 +1,16 @@
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15'];
+const mainCourseMenu = ['Steak- $50', 'Pasta- $20', 'Burger- $25', 'Salmon- $60'];
+const dessertMenu = ['Cake- $40', 'Ice Cream- $20', 'Pudding- $15', 'Fruit Salad- $20'];
+
+const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+let dessertItem = '';
+for (let i = 0; i < dessertMenu.length; i++) {
+    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
